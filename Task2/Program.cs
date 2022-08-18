3517e1c1b2a53a5537513dcf6900dd89da7ff5f0
Valintina <qwerty_6@mail.ru>
2022-08-18 13:45:21 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// == сравнить
+// = присвоить
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(a > b && a > c)
+
+{
+    max = a;
+}
+
+else if(b > a && b > c)
+{
+   max = b;
+}
+
+else if(c > a && b < c)
+{
+   max = c;
+}
+System.Console.WriteLine(max);
